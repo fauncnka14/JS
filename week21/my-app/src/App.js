62c2dbd9c5ed3b422088cae1d6b8af80af3109c5
fauncnka14 <annadorohkina14@gmail.com>
2025-01-19 21:42:21 +0300

    week 21

---
+++ week21/my-app/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import { Chart } from "react-google-charts";
+
+function App() {
+  const data = [
+    ["Task", "Hours per Day"],
+    ["Work", 9],
+    ["Eat", 2],
+    ["Commute", 2],
+    ["Watch TV", 2],
+    ["Sleep", 7],
+  ];
+
+  const options = {
+    title: "My Daily Activities",
+  };
+  return (
+    <div className="App">
+      <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"400px"}
+    />
+    </div>
+  );
+}
+
+export default App;
